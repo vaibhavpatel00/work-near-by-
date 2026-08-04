29868abe59e0c5b39cc0bcca185efa4c8960e1fe
vaibhavpatel00 <vp2007patel@gmail.com>
2026-08-04 16:38:20 +0530

    chore: update monetag sw.js to new zone 11499241

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11499163
+    "zoneId": 11499241
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
